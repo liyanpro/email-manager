9f2fb11b3bd98b571466f77cb36311406dfd8b29
liyan <liyan@qijiayoudao.com>
2019-09-11 19:08:05 +0800

    add log.base

--- src/main/java/cloud/liyan/emailmanager/interceptor/LoginCheckInterceptor.java
+++ src/main/java/cloud/liyan/emailmanager/interceptor/LoginCheckInterceptor.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
